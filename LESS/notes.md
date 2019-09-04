4eccc77f147f8174ac532b695a15b3894904ba7e
CScori <amilanotte1366@gmail.com>
2019-09-04 11:33:04 -0500

    starts menu function adds notes

---
+++ LESS/notes.md
@@ -0,0 +1,9 @@
+functin(param of data, param of data) {
+    const name = document.createElement('html'); -- repeat to make a card componenet
+
+    append the flow set up so it maks function
+    oNamr.classList.add('giveNme);
+
+    
+    name.classList.add = 'newName'; --name all the created elements 
+}
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -19,17 +19,21 @@ let menuItems = [
     </ul>
   </div>
 
-  The function takes an array as its only argument.
-
   Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
   Add those items to the <ul>
-
   Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
-
   Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
-
   Step 5: return the menu component.
-
   Step 6: add the menu component to the DOM.
   
 */
+ function comp = (menu, par) {
+   const drop = document.createElement('div');
+   const line = document.createElement('ul');
+   
+ };
+ <div class="menu">
+ <ul>
+   {each menu item as a list item}
+ </ul>
+</div>
